ef744bc6fadb507e546421fb5cfe86dac38606ee
Chenlai Shi <33204827+shichenlai@users.noreply.github.com>
2018-02-28 17:53:03 -0600

    Add files via upload

--- stage1/code/read.py
+++ stage1/code/read.py
@@ -1,35 +1,14 @@
-#!/usr/bin/env python
-
-"""
-Read text data into the document and organize them
-
-You need to change mypath at the begaining if you 
-run this program in your own computer
-
-
-"""
-
-# Global file path that must be changed/ensured
-
-mypath = "/Users/mingrenshen/Develop/CS839ClassProject/stage1/DataSet/B_Set/Data/"
-
-# import needed packages
 import re
 import csv
 from os import listdir
 from os.path import isfile, join
-
 #import nltk
 from nltk.corpus import stopwords
-
-# read in files and set up some fields and labels
+mypath = ""
 files = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f[-4:] == ".txt"]
-
 combo = [1,2,3,4]
 fields = ['docID','word', 'startPos', 'endPos', 'label','bag','preWord','postWord']
 stops = set(stopwords.words("english"))
-
-# write data into the final file
 with open("data.csv", 'w') as csvFile:
     csvWriter = csv.DictWriter(csvFile, fieldnames=fields)
     csvWriter.writeheader()
@@ -42,6 +21,7 @@ with open("data.csv", 'w') as csvFile:
             text = re.sub(r'[^\w\s]', "", text)
             text = re.sub(r'[^a-zA-Z]', " ", text)
             text = re.sub("\s\s+", " ", text)
+            #text = [w for w in text.split()]
             text = [w for w in text.split() if not w.lower() in stops]
             for n in combo:
                 Pos = 0
@@ -54,7 +34,7 @@ with open("data.csv", 'w') as csvFile:
                     if index==len(text)-n-1:
                         postWord="NA"
                     else:
-                        postWord=text[index+1]
+                        postWord=text[index+n]
                     for j in range(index, index+n):
                         word = word + text[j] + " "
                     word = word.rstrip()
